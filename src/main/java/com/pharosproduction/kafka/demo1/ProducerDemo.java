a9bd0538a14ad24fe944148a2111a16efb394b14
Dmytro Nasyrov <dmytro@pharosproduction.com>
2019-01-31 02:08:40 +0200

    add consumer

--- src/main/java/com/pharosproduction/kafka/demo1/ProducerDemo.java
+++ src/main/java/com/pharosproduction/kafka/demo1/ProducerDemo.java
@@ -1,26 +1,50 @@
 package com.pharosproduction.kafka.demo1;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class ProducerDemo {
 
     // psvm + Tab
-    public static void main(String[] args) {
-        String bootstrapServer = "127.0.0.1:9092";
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        final Logger logger = LoggerFactory.getLogger(ProducerDemo.class);
 
+        String bootstrapServer = "127.0.0.1:9092";
         Properties props = new Properties();
         props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
         props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
-        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello world");
-        producer.send(record);
+
+        for (int i = 0; i < 10; i++) {
+            String topic = "first_topic";
+            String value = "hello world " + i;
+            String key = "id+" + i;
+
+            logger.info("Key: " + key);
+
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, key, value);
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if (e != null) {
+                        logger.error("Error while producing", e);
+                        return;
+                    }
+
+                    logger.info("Received new meta. \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp());
+                }
+            }).get();
+        }
+
         producer.close();
     }
 }
